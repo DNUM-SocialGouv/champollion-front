6993ed363f7b46f496e5d8a7903b4c5b255886a2
Sara Bourdon <sara.bourdon@sg.social.gouv.fr>
2023-01-27 16:51:44 +0000

    feat(global): CHAM-98 add display theme modal

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,19 +2,54 @@ import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui"
 import { StyledEngineProvider } from "@mui/material/styles"
 import { Outlet } from "react-router-dom"
 
-import { AppHeader } from "../components/AppHeader"
-import AppFooter from "../components/AppFooter"
+import { Display, headerFooterDisplayItem } from "@codegouvfr/react-dsfr/Display"
+import { Footer } from "@codegouvfr/react-dsfr/Footer"
+import { Header } from "@codegouvfr/react-dsfr/Header"
+
+const brandTop = (
+  <>
+    Ministère
+    <br />
+    du travail,
+    <br />
+    du plein emploi
+    <br />
+    et de l'insertion
+  </>
+)
+
+const homeLinkProps = {
+  to: "/",
+  title: "Accueil Champollion - Ministère du travail, du plein emploi et de l'insertion",
+}
+
+const contentDescription =
+  "Champollion est un projet développé par les équipes de la Direction du numérique des ministères sociaux, en collaboration avec la Direction Générale du Travail (DGT)."
 
 export default function Root() {
   return (
     <StyledEngineProvider injectFirst>
       <MuiDsfrThemeProvider>
         <div className="App flex min-h-screen flex-col">
-          <AppHeader />
+          <Header
+            brandTop={brandTop}
+            homeLinkProps={homeLinkProps}
+            serviceTagline="Outil d'aide au contrôle précarité à destination de l'inspection du travail"
+            serviceTitle="Champollion"
+            quickAccessItems={[headerFooterDisplayItem]}
+          />
           <div className="flex flex-auto">
             <Outlet />
           </div>
-          <AppFooter />
+          <Footer
+            accessibility="partially compliant"
+            brandTop={brandTop}
+            className="fr-mt-10v"
+            contentDescription={contentDescription}
+            homeLinkProps={homeLinkProps}
+            bottomItems={[headerFooterDisplayItem]}
+          />
+          <Display />
         </div>
       </MuiDsfrThemeProvider>
     </StyledEngineProvider>
