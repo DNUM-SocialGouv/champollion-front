c5441a5a82fdb0ea57d96b53f424dfec0f368191
Sara Bourdon <sara.bourdon@sg.social.gouv.fr>
2023-02-10 15:04:55 +0000

    feat: CHAM-12CHAM-62 add raison sociale

--- src/routes/etablissement/index.tsx
+++ src/routes/etablissement/index.tsx
@@ -14,20 +14,22 @@ import EtabBanner from "../../components/EtabBanner"
 type EtabLoader = {
   siret: string
   pathname: string
+  raisonSociale: string
 }
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   const url = new URL(request.url)
   const siret = params.siret ? String(params.siret) : ""
-  const { ett } = await getEtablissementType(siret)
+  const { ett, raisonSociale } = await getEtablissementType(siret)
 
   if (ett) {
     return redirect(`/ett/${siret}`)
   }
 
   return {
-    siret,
     pathname: url.pathname,
+    raisonSociale,
+    siret,
   } as EtabLoader
 }
 
@@ -41,7 +43,7 @@ const tabIndex = {
 } as Record<string, number>
 
 export default function Etab() {
-  const { siret, pathname } = useLoaderData() as EtabLoader
+  const { pathname, raisonSociale, siret } = useLoaderData() as EtabLoader
   const navigate = useNavigate()
 
   const initialTab: string = pathname.includes("postes") ? "tab2" : "tab1"
@@ -56,7 +58,7 @@ export default function Etab() {
   return (
     <>
       <div className="flex w-full flex-col items-center">
-        <EtabBanner etabName={"MonEtab"} isEtt={false} siret={siret} />
+        <EtabBanner etabName={raisonSociale} isEtt={false} siret={siret} />
 
         <div className="fr-mt-2w fr-container">
           <Tabs selectedTabId={selectedTabId} tabs={tabs} onTabChange={handleTabChange}>
