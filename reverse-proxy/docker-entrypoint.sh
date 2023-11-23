1a6812e205c5927ddce52c0a6084cfc11b5ec8bf
leoguillaume <leo.guillaume@sg.social.gouv.fr>
2023-11-23 16:49:13 +0100

    feat(deploy): add monitoring location

--- reverse-proxy/docker-entrypoint.sh
+++ reverse-proxy/docker-entrypoint.sh
@@ -4,5 +4,6 @@ set -eu
 envsubst '${AUTH_URL}' < /etc/nginx/conf.d/auth.location.template > /etc/nginx/conf.d/auth.location
 envsubst '${APP_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 envsubst '${APP_URL}' <  /etc/nginx/snippets/ssl-cert.conf.template > /etc/nginx/snippets/ssl-cert.conf
+envsubst '${IPVM_PRODUCTION_ENS224_PLACEHOLDER}' < /etc/nginx/conf.d/monitoring.location.template > /etc/nginx/conf.d/monitoring.location
 
 exec "$@"
