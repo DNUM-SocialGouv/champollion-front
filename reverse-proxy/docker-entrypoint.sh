58a2d6b180704fcf0ba1bd808d5787f031e521f0
leoguillaume <leo.guillaume@sg.social.gouv.fr>
2024-01-09 10:40:12 +0100

    fix: keycloak realm changing

--- reverse-proxy/docker-entrypoint.sh
+++ reverse-proxy/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
+envsubst '${KEYCLOAK_REALM}' < /etc/nginx/conf.d/app.location.template > /etc/nginx/conf.d/app.location
 envsubst '${AUTH_URL}' < /etc/nginx/conf.d/auth.location.template > /etc/nginx/conf.d/auth.location
 envsubst '${APP_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 envsubst '${IPVM_PRODUCTION_ENS224_PLACEHOLDER}' < /etc/nginx/conf.d/monitoring.location.template > /etc/nginx/conf.d/monitoring.location
